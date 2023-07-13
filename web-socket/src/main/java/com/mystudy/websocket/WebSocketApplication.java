086c089c93b9aea0810d933fe5489c750759fc65
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-07-13 17:17:32 +0900

    build: 프로젝트 생성

---
+++ web-socket/src/main/java/com/mystudy/websocket/WebSocketApplication.java
@@ -0,0 +1,18 @@
+package com.mystudy.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/* Reference By
+* https://www.youtube.com/watch?v=XY5CUuE6VOk
+* */
+
+
+@SpringBootApplication
+public class WebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketApplication.class, args);
+	}
+
+}
