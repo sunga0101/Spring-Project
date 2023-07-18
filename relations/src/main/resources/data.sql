b5269d2bce4bd94a2357b77938e4d1e28e4f4da5
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-07-18 13:44:17 +0900

    chore: 설정 추가

---
+++ relations/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into instructor_entity (first_name, last_name)
+values ('jeeho', 'park');
+
+insert into lecture_entity(day, name, start_time, end_time)
+values ('tue', 'Database', 11, 16), ('tue', 'Algorithm', 9, 11), ('wed', 'Spring', 11, 16);
