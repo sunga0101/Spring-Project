749f40aa10c701e9ddec69ff7c30cdf9c2df5c8d
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-07-18 13:09:41 +0900

    build: 프로젝트 생성

---
+++ auth-study/src/main/java/com/example/auth/RootController.java
@@ -0,0 +1,27 @@
+package com.example.auth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+    @GetMapping("")
+    public String root(){
+        return "hello"; // 로그인 한 사용자만 볼 수 있는 페이지
+    }
+
+    // 사용자 인증 없이 접근 가능
+    @GetMapping("no-auth")
+    public String noAuth(){
+
+        return "사용자 인증 없이 접근 가능한 no auth";
+    }
+
+    // 인증받은 사용자만 접근 가능
+    @RequestMapping("require-auth")
+    public String requireAuth(){
+        return "인증받은 사용자만 접근 가능한 require-auth";
+    }
+
+}
---
+++ auth-study/src/main/java/com/example/auth/UserController.java
@@ -0,0 +1,20 @@
+package com.example.auth;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j
+//@Controller  // 로그인 페이지를 보여줄려고
+//@RequestMapping("/users")
+public class UserController {
+
+}
