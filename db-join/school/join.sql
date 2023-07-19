65b566b9fa7565a3c34c6480f9a7a219e84665aa
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-07-19 14:25:27 +0900

    feat: 다대다, 서브쿼리 구현

--- db-join/school/join.sql
+++ db-join/school/join.sql
@@ -55,3 +55,65 @@ ON lecture.instructor_id = instructor.id;
 SELECT *
 FROM student FULL JOIN instructor
 ON student.advisor_id = instructor.id;
+
+-- MANY TO MANY
+SELECT *
+FROM student
+         INNER JOIN enrolling_lectures
+                    ON student.id = enrolling_lectures.student_id
+         INNER JOIN lecture
+                    ON enrolling_lectures.lecture_id = lecture.id;
+
+
+SELECT *
+FROM student
+         LEFT OUTER JOIN enrolling_lectures
+                         ON student.id = enrolling_lectures.student_id
+         LEFT OUTER JOIN lecture
+                         ON enrolling_lectures.lecture_id = lecture.id;
+
+
+SELECT *
+FROM lecture
+         LEFT OUTER JOIN enrolling_lectures
+                         ON lecture.id = enrolling_lectures.lecture_id
+         LEFT OUTER JOIN student
+                         ON enrolling_lectures.student_id = student.id;
+
+-- 화요일에 강의를 하는 교수님들만 뽑아보자
+SELECT lecture.name, lecture.day, instructor.first_name, instructor.last_name
+FROM lecture INNER JOIN instructor
+                        ON lecture.instructor_id = instructor.id
+WHERE lecture.day = 'tue';
+
+SELECT l.name, l.day, i.first_name, i.last_name
+FROM lecture as l INNER JOIN instructor i
+                             ON l.instructor_id = i.id
+WHERE l.day = 'tue';
+
+
+-- subquery
+SELECT s.id AS student_id,
+       s.first_name,
+       s.last_name,
+       subquery.name
+FROM student s
+         JOIN (
+    SELECT el.student_id, l.name
+    FROM enrolling_lectures el
+             JOIN lecture l on el.lecture_id = l.id
+    WHERE l.instructor_id = 2
+) subquery ON s.id = subquery.student_id
+ORDER BY s.id;
+
+SELECT s.id AS student_id,
+       s.first_name,
+       s.last_name
+FROM student s
+WHERE s.id IN (
+    SELECT el.student_id
+    FROM enrolling_lectures el
+             JOIN lecture l on el.lecture_id = l.id
+             JOIN instructor i on i.id = l.instructor_id
+    WHERE i.id = 2
+);
