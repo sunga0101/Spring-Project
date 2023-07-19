53ef540a6c38a98c45e197a3e4f7812a9f062d62
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-07-19 19:13:52 +0900

    feat: 프로젝트 빌드

---
+++ security-practice/src/main/java/com/example/securitypractice/SecurityPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityPracticeApplication.class, args);
+	}
+
+}
