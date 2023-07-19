bc6d04071d3ec6883f8e016c89b893e33968a545
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-07-19 14:21:02 +0900

    feat: inner join 구현

--- db-join/school/join.sql
+++ db-join/school/join.sql
@@ -8,3 +8,21 @@ FROM lecture, student;
 SELECT *
 FROM lecture, instructor
 WHERE lecture.instructor_id = instructor.id;
+
+-- INNER JOIN
+SELECT *
+FROM lecture INNER JOIN instructor
+                        ON lecture.instructor_id = instructor.id;
+
+SELECT *
+FROM student INNER JOIN instructor
+                        ON student.advisor_id = instructor.id;
+
+SELECT *
+FROM student INNER JOIN enrolling_lectures
+                        ON student.id = enrolling_lectures.student_id;
+
+-- INNER 생략
+SELECT *
+FROM lecture JOIN instructor
+                  ON lecture.instructor_id = instructor.id;
