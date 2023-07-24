329e8ffc2d321a519c3727568013d9ef1d042edb
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-07-24 15:50:44 +0900

    feat: 함수표현식 예제 추가

--- etc/javascript-practice/04_function.js
+++ etc/javascript-practice/04_function.js
@@ -3,3 +3,35 @@
 function add (num1, num2){
     return num1 + num2
 }
+
+function addLog (num1, num2){
+    console.log(num1 + num2)
+}
+
+console.log(add(10,20))
+console.log(addLog(15,15))
+
+// 함수 표현식
+// 표현식 : 어떤 특정한 값으로 귀결되는 코드
+// 변수를 선언하고, 해당 변수에 함수를 할당하는 방식
+const subtract = function (num1,num2) {
+    return num1-num2
+}
+
+// 함수 인자 전달하기
+// 1. 인자 기본값
+const greeting = function(name = 'Anonymous') {
+    return `Hi, ${name}`
+}
+console.log(greeting('Jeeho'))
+console.log(greeting())
+
+// 2. 매개변수와 인자의 갯수 불일치 허용
+const divide = function (num1, num2) {
+    return num1 / num2
+}
+
+console.log(divide())
+console.log(divide(1))
+console.log(divide(1, 2))
+console.log(divide(1, 2, 3))
